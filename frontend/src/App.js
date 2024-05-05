6efe9297a3fb10cc97fa52c9d92cd43f54863bd3
derek-mo <derek.mo7203@gmail.com>
2024-05-04 21:22:19 -0400

    nexusdb init commit

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import DataTable from './Components/DataTable/DataTable'; // Assuming CarTable.js is in the same directory
+
+function App() {
+    return (
+        <>
+            <body>
+                <h1 className='site-name'>Welcome to NexusDB</h1>
+            </body>
+
+            <DataTable />
+        </>
+    );
+}
+
+export default App;
+
